e816a972eee5b00a7b0df995fbeccf7186a8a63c
brockar <55927935+brockar@users.noreply.github.com>
2020-10-04 21:51:12 -0300

    asd

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1474,7 +1474,7 @@
 
 // Preheat Constants
 #define PREHEAT_1_LABEL       "PLA"
-#define PREHEAT_1_TEMP_HOTEND 210
+#define PREHEAT_1_TEMP_HOTEND 211
 #define PREHEAT_1_TEMP_BED     60
 #define PREHEAT_1_FAN_SPEED     0 // Value from 0 to 255
 
