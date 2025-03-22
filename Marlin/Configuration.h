79b082e8a727d254f434dd2365d59a0f8c70f302
brockar <martinnguzman.mg@gmail.com>
2025-03-22 20:06:04 -0300

    Update author information in Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -61,7 +61,8 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Original author or contributor.
+//#define STRING_CONFIG_H_AUTHOR "(none, default config)" // Original author or contributor.
+#define STRING_CONFIG_H_AUTHOR "Martin" // Original author or contributor.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 // @section machine
