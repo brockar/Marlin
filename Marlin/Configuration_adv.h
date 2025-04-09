5db06a11226c5b5bcd30116a137062b75d68de03
brockar <martinnguzman.mg@gmail.com>
2025-04-08 22:59:34 -0300

    comment on stealchop e

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -3322,7 +3322,7 @@
     #define STEALTHCHOP_U
     #define STEALTHCHOP_V
     #define STEALTHCHOP_W
-    #define STEALTHCHOP_E
+    #define STEALTHCHOP_E //!To-do desactivar para lineal advance (no necesario actualmente)
   #endif
 
   /**
